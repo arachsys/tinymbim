c946942728d2b97c0ae988c5a72ee6d6097ba514
Chris Webb <chris@arachsys.com>
2022-07-24 15:33:24 +0100

    Improve usage message
    Add a brief description of each supported command to the usage message
    printed by tinymbim when it is run with no device or no valid command.

--- main.c
+++ main.c
@@ -31,21 +31,23 @@ int main(int argc, char **argv) {
   fprintf(stderr, "\
 Usage: %s DEVICE COMMAND\n\
 Commands:\n\
-  open\n\
-  close\n\
-  caps\n\
-  subscriber\n\
-  radio [on|off]\n\
-  pin [PIN]\n\
-  home\n\
-  visible\n\
-  register\n\
-  attach\n\
-  detach\n\
-  signal\n\
-  connect APN [AUTH USER PASS]\n\
-  disconnect\n\
-  config\n\
+  open              initialise modem\n\
+  close             shut down modem\n\
+  caps              query device capabilities\n\
+  subscriber        query subscriber ready-state\n\
+  radio [on|off]    query or control radio power\n\
+  pin [PIN]         query PIN status or unlock with PIN\n\
+  home              query home provider details\n\
+  visible           scan for visible providers\n\
+  register [auto|PROVIDER]\n\
+                    query or set provider registration state\n\
+  attach            attach to packet service\n\
+  detach            detach from packet service\n\
+  signal            check signal strength and error rate\n\
+  connect [ipv4:|ipv6:|ipv4v6:]APN [AUTH USER PASS]\n\
+                    activate IP data session\n\
+  disconnect        deactivate IP data session\n\
+  config            query IP configuration for data session\n\
 ", argv[0]);
   return 64;
 }
